11f18e4c6d7ce1578cbda88f3d97729d55c009f1
hiddenntreasure <mdkito@yahoo.com>
2020-06-11 21:05:47 +0600

    EHI-52 Python Basic: list

--- src/main/data_structure/list_exercise.py
+++ src/main/data_structure/list_exercise.py
@@ -3,5 +3,16 @@ def find_average(list_of_number):
     summation = 0
     for number in list_of_number:
         summation = summation + number
+    print('Average: ',summation / len(list_of_number))
 
     return summation / len(list_of_number)
+
+def sort_list(list_1):
+    
+    sorted_lst = sorted(list_1)
+    print("Sorted list :", sorted_lst)
+    
+    return sorted_lst
+    
+
+
--- src/test/data_structure/list_exercise_test.py
+++ src/test/data_structure/list_exercise_test.py
@@ -1,10 +1,18 @@
 import unittest
 
 from src.main.data_structure.list_exercise import find_average
-
+from src.main.data_structure.list_exercise import sort_list
 
 class TestList(unittest.TestCase):
 
     def test_find_average(self):
         self.assertEqual(find_average([1, 2, 3]), 2)
         self.assertEqual(find_average([1, 2 , 3, 4, 5, 6, 7, -1]), 3.375)
+        
+class SortTest(unittest.TestCase):
+    
+    def test_sort_list(self):
+        self.assertEqual(sort_list([3,5,1,9]),[1,3,5,9])
+
+if __name__ == '__main__':
+    unittest.main()

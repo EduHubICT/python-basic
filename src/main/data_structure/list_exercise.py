fec2777c57b1a15751f33102dc7fc096297496a2
RijviZ <rijvizaman8@gmail.com>
2020-06-11 18:12:38 +0600

    Practicing List

--- src/main/data_structure/list_exercise.py
+++ src/main/data_structure/list_exercise.py
@@ -1,7 +1,26 @@
+#List_Operations
 
-def find_average(list_of_number):
-    summation = 0
-    for number in list_of_number:
-        summation = summation + number
+#Simple List
+list1=[1, 2, 3,'Rijvi', 4, 5]
+print(list1)
 
-    return summation / len(list_of_number)
+#Appends a new element to the end of the list
+list1.append(6)
+print(list1)
+
+# Appending a list to another list
+list2=[7,8]
+list1.append(list2)
+print(list1)
+print(list1[6])
+
+# Extend list by appending all elements from list2
+list1.extend(list2)
+print(list1)
+
+#List Concatanations
+print(list2+[9,10]+['Zaman'])
+
+# insert 5 at position 5
+list1.insert(5, 5)
+print(list1)

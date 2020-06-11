f7576236bb97f3a1392c17911ee97120b9a85aa4
jonyroy <jonyroyacc@gmail.com>
2020-06-11 20:21:38 +0600

    [EHI-54] create exercise branch

--- src/main/data_structure/list_exercise.py
+++ src/main/data_structure/list_exercise.py
@@ -1,54 +1,23 @@
 
 def find_average(list_of_number):
-    summation = 0
-    for number in list_of_number:
-        summation = summation + number
-
-    return summation / len(list_of_number)
+    pass # TODO
 
 
 def find_max(list_of_number):
-    max_val = -1000000
-    for number in list_of_number:
-        if number > max_val:
-            max_val = number
-    return max_val
+    pass
 
 
 def find_min(list_of_number):
-    min_val = 1000000
-    for number in list_of_number:
-        if number < min_val:
-            min_val = number
-    return min_val
+    pass
 
 
 def find_sum(list_of_number) -> int:
-    summation = 0
-    for number in list_of_number:
-        summation = summation + number
-    return summation
+    pass
 
 
 def sort_list_bubble_ass(list_of_number):
-    list_length = len(list_of_number)
-    for index in range(0, list_length - 1):
-        for ind in range(0, list_length - index - 1):
-            if list_of_number[ind] > list_of_number[ind + 1]:
-                temp = list_of_number[ind]
-                list_of_number[ind] = list_of_number[ind + 1]
-                list_of_number[ind + 1] = temp
-
-    return list_of_number
+    pass
 
 
 def sort_list_bubble_dsc(list_of_number):
-    list_length = len(list_of_number)
-    for index in range(0, list_length - 1):
-        for ind in range(0, list_length - index - 1):
-            if list_of_number[ind] < list_of_number[ind + 1]:
-                temp = list_of_number[ind]
-                list_of_number[ind] = list_of_number[ind + 1]
-                list_of_number[ind + 1] = temp
-
-    return list_of_number
+    pass

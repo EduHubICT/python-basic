d60394386438da48964027e16277b714e5f6776b
hiddenntreasure <mdkito@yahoo.com>
2020-06-12 01:38:24 +0600

    Revert "[EHI-52] Testing conflict"
    This reverts commit c88ec9f99f3d0c58f4fad49264202315780d965d.

--- src/main/data_structure/list_exercise.py
+++ src/main/data_structure/list_exercise.py
@@ -20,7 +20,7 @@ def slicing(list1):
     lst = []
     i = 0
     for index in range(lst_len):
-        if index > 1:
+        if index > 2:
             lst.append(list1[index])
             i+=1
     return lst

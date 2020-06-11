c9334f46a695d867d3b32c92418424897bf03b14
Ahad726 <atmahad726@gmail.com>
2020-06-11 22:01:16 +0600

    EHI-53: practice python list

--- src/main/data_structure/list_exercise.py
+++ src/main/data_structure/list_exercise.py
@@ -5,3 +5,16 @@ def find_average(list_of_number):
         summation = summation + number
 
     return summation / len(list_of_number)
+
+
+def even_finder(list_of_number):
+    even_list = []
+    for number in list_of_number :
+        if number % 2 ==0 :
+            even_list.append(number)
+                    
+    return even_list
+
+numbers = even_finder([10,4,3,5,7,9,12,16])
+
+print(numbers)
--- src/test/data_structure/list_exercise_test.py
+++ src/test/data_structure/list_exercise_test.py
@@ -3,8 +3,21 @@ import unittest
 from src.main.data_structure.list_exercise import find_average
 
 
+
 class TestList(unittest.TestCase):
 
     def test_find_average(self):
         self.assertEqual(find_average([1, 2, 3]), 2)
         self.assertEqual(find_average([1, 2 , 3, 4, 5, 6, 7, -1]), 3.375)
+    
+
+    
+
+from src.main.data_structure.list_exercise import even_finder
+
+class Test_List(unittest.TestCase):
+
+
+    def test_even_finder(self):
+        self.assertEqual(even_finder([5,3,2,6,0,1,10,56]),[2,6,0,10,56])
+        self.assertEqual(even_finder([3,4,7,9,10]),[4,10])

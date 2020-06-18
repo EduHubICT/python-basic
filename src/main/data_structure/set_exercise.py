3bb24d36a1cc1670ffbf6224fd8d907bc075e7b6
hiddenntreasure <mdkito@yahoo.com>
2020-06-18 22:52:00 +0600

    [EHI-52 moderate set files]

--- src/main/data_structure/set_exercise.py
+++ src/main/data_structure/set_exercise.py
@@ -1,13 +1,19 @@
-<<<<<<< HEAD
 from src.main.logger.py_logger import PyLogger
 
 
 logger = PyLogger.get_configured_logger()
+    
+"""
+Set contain only unique value. No duplicate is possible. 
+set is immutable but mutable as whole.
+{} - this is an empty dict, but
+{0, 1} - this is a set; 0, 1 are elements
+set() - to define an empty set
+"""
 
-# set contain only unique value. No duplicate is possible. 
-# set is immutable but mutable as whole
 
-def create_unique_value(list_of_number):
+# convert list into set, which contain unique value only
+def create_unique_value_set(list_of_number):
     unique_values = set(list_of_number)
 
     logger.info('Unique value representation: {}'.format(unique_values))
@@ -15,13 +21,15 @@ def create_unique_value(list_of_number):
     return unique_values
 
 
-def accessing_value_in_set(set_of_number, value):
+# return yes if expected value contains in the set
+def access_value_in_set(set_of_number, value):
     for number in set_of_number:
         if number == value:
             logger.info('{} is in the set'.format(value))
             
             return 'yes'
-        
+
+# add a value in the set or remove a value
 def add_remove_in_set(set_of_number, add_value, remove_value):
     if add_value is not None:
         set_of_number.add(add_value)
@@ -34,55 +42,6 @@ def add_remove_in_set(set_of_number, add_value, remove_value):
         logger.info('New set after removing a value: {}'.format(set_of_number))
         
         return 'Remove'
-    
-    
-def union_intersection_in_set(set_of_number_1, set_of_number_2, u_i):
-    if u_i == 'Union':
-        new_set_union = set_of_number_1 | set_of_number_2
-        logger.info('New Set after union: {}'.format(new_set_union))
-        
-        return 'Union'
-    
-    elif u_i == 'Intersection':
-        new_set_intersection = set_of_number_1 & set_of_number_2
-        logger.info('New Set after intersection: {}'.format(new_set_intersection))
-        
-        return 'Intersection'
-    
-    else:
-        logger.info('No operation is passed')
-        return None
-    
-    
-def difference_compare_in_set(set_of_number_1, set_of_number_2, u_i):
-    if u_i == 'Difference':
-        new_set = set_of_number_1 - set_of_number_2
-        logger.info('New Set after Difference: {}'.format(new_set))
-        
-        return 'Difference'
-    
-    elif u_i == 'Compare':
-        if (set_of_number_1 <= set_of_number_2):
-            logger.info('Subset')
-            
-            return 'Subset'
-        else:
-            logger.info('Superset')
-            
-            return 'Superset'
-    
-    else:
-        logger.info('No operation is passed')
-        return None
-    
-            
-        
-=======
-"""
-{} - this is an empty dict, but
-{0, 1} - this is a set; 0, 1 are elements
-set() - to define an empty set
-"""
 
 
 # return new set containing common and uncommon items between set_a and set_b
@@ -139,4 +98,3 @@ def find_supersets(set_a, set_b) -> bool:
             return False
 
     return True
->>>>>>> ce0055878cd154249e92bfc24019a31b879491d6

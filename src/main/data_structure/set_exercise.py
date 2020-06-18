54160d9b3e46b58ccb8e9647577266bb1b6835ec
hiddenntreasure <mdkito@yahoo.com>
2020-06-18 13:33:52 +0600

    [EHI-52 Set Completed]

--- src/main/data_structure/set_exercise.py
+++ src/main/data_structure/set_exercise.py
@@ -3,6 +3,8 @@ from src.main.logger.py_logger import PyLogger
 
 logger = PyLogger.get_configured_logger()
 
+# set contain only unique value. No duplicate is possible. 
+# set is immutable but mutable as whole
 
 def create_unique_value(list_of_number):
     unique_values = set(list_of_number)
@@ -10,3 +12,67 @@ def create_unique_value(list_of_number):
     logger.info('Unique value representation: {}'.format(unique_values))
 
     return unique_values
+
+
+def accessing_value_in_set(set_of_number, value):
+    for number in set_of_number:
+        if number == value:
+            logger.info('{} is in the set'.format(value))
+            
+            return 'yes'
+        
+def add_remove_in_set(set_of_number, add_value, remove_value):
+    if add_value is not None:
+        set_of_number.add(add_value)
+        logger.info('New set after adding a value: {}'.format(set_of_number))
+        
+        return 'Add'
+    
+    elif remove_value is not None:
+        set_of_number.discard(remove_value)
+        logger.info('New set after removing a value: {}'.format(set_of_number))
+        
+        return 'Remove'
+    
+    
+def union_intersection_in_set(set_of_number_1, set_of_number_2, u_i):
+    if u_i == 'Union':
+        new_set_union = set_of_number_1 | set_of_number_2
+        logger.info('New Set after union: {}'.format(new_set_union))
+        
+        return 'Union'
+    
+    elif u_i == 'Intersection':
+        new_set_intersection = set_of_number_1 & set_of_number_2
+        logger.info('New Set after intersection: {}'.format(new_set_intersection))
+        
+        return 'Intersection'
+    
+    else:
+        logger.info('No operation is passed')
+        return None
+    
+    
+def difference_compare_in_set(set_of_number_1, set_of_number_2, u_i):
+    if u_i == 'Difference':
+        new_set = set_of_number_1 - set_of_number_2
+        logger.info('New Set after Difference: {}'.format(new_set))
+        
+        return 'Difference'
+    
+    elif u_i == 'Compare':
+        if (set_of_number_1 <= set_of_number_2):
+            logger.info('Subset')
+            
+            return 'Subset'
+        else:
+            logger.info('Superset')
+            
+            return 'Superset'
+    
+    else:
+        logger.info('No operation is passed')
+        return None
+    
+            
+        
--- src/test/data_structure/set_test.py
+++ src/test/data_structure/set_test.py
@@ -1,13 +1,28 @@
 import unittest
 
 from src.main.data_structure.set_exercise import *
-#from src.main.data_structure.list_exercise import *
+
 
 class TestSet(unittest.TestCase):
     def test_unique_value(self):
         self.assertEqual(create_unique_value([ 1, 1, 2, 4, 3, 5, 3]), { 1, 2, 3, 4, 5})
-        #self.assertEqual(find_average([1, 2, 3, 4, 5, 6, 7, -1]), 3.375)
-        # self.assertEqual(find_average([1, 2, 3]), 2)
+
+    def test_accessing_value_in_set(self):
+        self.assertEqual(accessing_value_in_set({1, 3, 5, 6, 8, 9}, 3), 'yes')
+       
+    def test_add_remove_in_set(self):
+        self.assertEqual(add_remove_in_set({1, 3, 5, 6, 8, 9}, 2, None), 'Add')
+        self.assertEqual(add_remove_in_set({1, 3, 5, 6, 8, 9}, None, 9), 'Remove')
+        
+    def test_union_intersection_in_set(self):
+        self.assertEqual(union_intersection_in_set({1, 3, 5}, {2, 4, 6}, 'Union'), 'Union')
+        self.assertEqual(union_intersection_in_set({1, 3, 5}, {2, 3, 6}, 'Intersection'), 'Intersection')
+        
+    def test_difference_compare_in_set(self):
+        self.assertEqual(difference_compare_in_set({1, 3, 5}, {2, 4, 6}, 'Difference'), 'Difference')
+        self.assertEqual(difference_compare_in_set({1, 3, 5}, {1, 3, 5, 7, 9}, 'Compare'), 'Subset')
+        self.assertEqual(difference_compare_in_set({1, 3, 5, 7, 9, 11,13}, {1, 3, 5, 7, 9}, 'Compare'), 'Superset')
+
 
 if __name__ == '__main__':
     unittest.main()

15c42bc575218b4660c278c6be1ca396a32cd3c0
imshakil <mh.ice.iu@gmail.com>
2020-06-28 20:59:43 +0600

    [EHI-65: lambda related prbolem solved from w3resource]

--- src/main/data_structure/lambda_exercise.py
+++ src/main/data_structure/lambda_exercise.py
@@ -2,6 +2,7 @@ from src.main.logger.py_logger import PyLogger
 
 logger = PyLogger.get_configured_logger()
 
+
 # lambda is a anonymous function. Which can take any number of
 # argument, but only one expression is possible.
 
@@ -19,7 +20,7 @@ def find_square_of_value():
 def multiply_by_two(list_of_number):
     multiply = list(map(lambda x: x * 2, list_of_number))
     logger.info('Multiply given number by two: {}'.format(multiply))
-    
+
     return multiply
 
 
@@ -27,11 +28,11 @@ def multiply_by_two(list_of_number):
 def find_even_numbers(list_of_number):
     even_numbers = list(filter(lambda x: (x % 2 == 0), list_of_number))
     logger.info('Even Numbers: {}'.format(even_numbers))
-    
+
     return even_numbers
 
 
-#closure is used
+# closure is used
 def add_two_value(y):
     second_value_add = lambda x: x + y
     logger.info('Two value is add: {}'.format(second_value_add))
@@ -39,14 +40,13 @@ def add_two_value(y):
     return second_value_add
 
 
-high_order_function = lambda x, function : x + function(x)
+high_order_function = lambda x, function: x + function(x)
 
-
-even_or_odd =  lambda x : (x % 2 and 'odd' or 'even')
+even_or_odd = lambda x: (x % 2 and 'odd' or 'even')
 
 
 def sort_id_number(ids):
     sort_number = sorted(ids, key=lambda x: int(x[2:]))
     logger.info('Sorted id number : {}'.format(sort_number))
-    
+
     return sort_number
---
+++ src/main/w3resource_problem_solved/python_lambda_exercise.py
@@ -0,0 +1,124 @@
+"""
+Resource link: https://www.w3resource.com/python-exercises/lambda/index.php
+Tried to solve most problem that i like to solve
+unit test file added in test section
+"""
+
+""" 
+1. Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument,
+    also create a lambda function that multiplies argument x with argument y and print the result.
+"""
+
+
+def simple_addition(x):
+    add = lambda a: a + 15
+    return add(x)
+
+
+def simple_multiplication(a, b):
+    mul = lambda x, y: x * y
+    return mul(a, b)
+
+
+""" 
+2. Write a Python program to create a function that takes one argument, 
+    and that argument will be multiplied with an unknown given number 
+"""
+
+
+def anonymous_multiplication(n):
+    return lambda x: x * n
+
+
+"""
+3. Write a Python program to sort a list of tuples using Lambda.
+"""
+
+
+def sort_tuples_list(list_of_tuples):
+    list_of_tuples.sort(key=lambda x: x[1])
+    return list_of_tuples
+
+
+"""
+4. Write a Python program to sort a list of dictionaries using Lambda.
+"""
+
+
+def sort_dict_list(list_of_dict):
+    list_of_dict.sort(key=lambda x: x['Age'])
+    return list_of_dict
+
+
+"""
+5. Write a Python program to filter a list of integers using Lambda.
+"""
+
+
+def get_odd_numbers_only(list_of_numbers):
+    return list(filter(lambda x: x % 2 != 0, list_of_numbers))
+
+
+def get_even_numbers_only(list_of_numbers):
+    return list(filter(lambda x: x % 2 == 0, list_of_numbers))
+
+
+"""
+6. Write a Python program to square and cube every number in a given list of integers using Lambda.
+"""
+
+
+def square_list_item(list_of_numbers):
+    return list(map(lambda x: x * x, list_of_numbers))
+
+
+def cube_list_item(list_of_numbers):
+    return list(map(lambda x: x * x * x, list_of_numbers))
+
+
+"""
+7. Write a Python program to find if a given string starts with a given character using Lambda.
+"""
+
+
+def find_word_with_character(word, char):
+    match = lambda x: True if x.startswith(char) else False
+    return match(word)
+
+
+"""
+10. Write a Python program to create Fibonacci series upto n using Lambda.
+"""
+
+
+def get_fibonacci_series(n):
+    fib_series = [0, 1]  # pre define
+    any(map(lambda _: fib_series.append(fib_series[-2] + fib_series[-1]), range(2, n)))
+    return fib_series
+
+
+"""
+11. Write a Python program to find intersection of two given arrays using Lambda.
+"""
+
+
+def find_intersection(list1, list2):
+    return list(filter(lambda x: x in list1, list2))
+
+
+"""
+17. Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.
+"""
+
+
+def find_divisible_numbers(list_of_numbers, divisor1, divisor2):
+    return list(filter(lambda x: (x % divisor1 == 0 or x % divisor2 == 0), list_of_numbers))
+
+
+"""
+18. Write a Python program to find palindromes in a given list of strings using Lambda.
+"""
+
+
+def find_palindromes(list_of_words):
+    return list(filter(lambda x: (x == x[::-1]), list_of_words))

70b1f0bf190117f785bdd456fcdb77193eeb08e3
Atm Ahad <atmahad726@gmail.com>
2020-06-29 20:32:14 +0600

    [EHI-53] string exercise added

---
+++ src/main/data_structure/string_exercise.py
@@ -0,0 +1,71 @@
+from src.main.logger.py_logger import PyLogger
+
+logger = PyLogger.get_configured_logger()
+
+
+# Count the number of characters (character appearance) in a string
+def char_appearance(string):
+    char_dict = {}
+    for n in string:
+        keys = char_dict.keys()
+        if n in keys:
+            char_dict[n] += 1
+        else:
+            char_dict[n] = 1
+    return char_dict
+
+
+#  Add 'ing' at the end of a given string (length should be at least 3).
+#  If the given string already ends with 'ing' then add 'ly' instead.
+#  If the string length of the given string is less than 3, leave it unchanged
+def add_string(string):
+    length = len(string)
+    if length > 2:
+        if string[-3:] == 'ing':
+            string += 'ly'
+        else:
+            string += 'ing'
+    return string
+
+
+# Find the first appearance of the substring 'not' and 'poor' from a given string,
+# if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'.
+# Return the resulting string
+def string_replace(string):
+    string_not = string.find('not')
+    string_poor = string.find('poor')
+
+    if string_poor > string_not > 0 and string_poor > 0:
+        # replace 'not...poor' with 'good'
+        string = string.replace(string[string_not:(string_poor + 4)], 'good')
+        return string
+    else:
+        return string
+
+
+# takes a list of words and returns the length of the longest one
+def find_longest_word(words_list):
+    word_len = []
+    for n in words_list:
+        word_len.append((len(n), n))
+    word_len.sort()
+    return word_len[-1][1]
+
+
+# Remove the nth index character from a nonempty string
+def remove_char(string, n):
+    first_part = string[:n]  # slice the string upto nth index
+    last_part = string[n + 1:]  # slice the string after nth index
+    return first_part + last_part  # concatenate the two slice
+
+
+# Count the occurrences of each word in a given sentence
+def count_word(string):
+    counts = dict()
+    words = string.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
---
+++ src/test/data_structure/string_exercise_test.py
@@ -0,0 +1,35 @@
+import unittest
+from src.main.data_structure.string_exercise import *
+
+
+class TestList(unittest.TestCase):
+    def test_char_appearance(self):
+        self.assertEqual(char_appearance('google.com'), {'o': 3, '.': 1, 'g': 2, 'l': 1, 'e': 1, 'c': 1, 'm': 1})
+        self.assertEqual(char_appearance('Dog'), {'D': 1, 'o': 1, 'g': 1})
+        self.assertEqual(char_appearance('potato'), {'p': 1, 'o': 2, 't': 2, 'a': 1})
+
+    def test_add_string(self):
+        self.assertEqual(add_string('ab'), 'ab')
+        self.assertEqual(add_string('abc'), 'abcing')
+        self.assertEqual(add_string('string'), 'stringly')
+
+    def test_string_replace(self):
+        self.assertEqual(string_replace('The lyrics is not that poor!'), 'The lyrics is good!')
+        self.assertEqual(string_replace('The lyrics is poor!'), 'The lyrics is poor!')
+
+    def test_find_longest_word(self):
+        self.assertEqual(find_longest_word(["PHP", "Exercises", "Backend"]), 'Exercises')
+        self.assertEqual(find_longest_word(['Dhaka', 'khunla', 'Chattogram', 'barisal']), 'Chattogram')
+
+    def test_remove_char(self):
+        self.assertEqual(remove_char('Python', 0), 'ython')
+        self.assertEqual(remove_char('ASPNET', 4), 'ASPNT')
+        self.assertEqual(remove_char('Laravel', 3), 'Larvel')
+
+    def test_count_word(self):
+        self.assertEqual(count_word('the quick brown fox jumps over the lazy dog.'), {'the': 2, 'jumps': 1, 'brown': 1, 'lazy': 1, 'fox': 1, 'over': 1, 'quick': 1, 'dog.': 1})
+        self.assertEqual(count_word('We are the Champion, We are the'), {'We': 2, 'are': 2, 'the': 2, 'Champion,': 1})
+
+
+if __name__ == '__main__':
+    unittest.main()

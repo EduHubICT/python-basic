1bd9ede16f44587f3f377290d9abb2b56b609229
imshakil <mh.ice.iu@gmail.com>
2020-07-11 13:24:55 +0600

    [EHI-75: w3resource string exercise solved]

--- src/main/opening_jobs_scraping/scrape_jobs.py
+++ src/main/opening_jobs_scraping/scrape_jobs.py
@@ -9,13 +9,13 @@ def scrap_companies():
         name = company["companyName"]
         url = company["url"]
 
-        if not company['jobs_list'] or not company['jobs_list']['tag']:
+        if not company["jobs_list"]["tag"]:
             continue
 
         try:
-            tag = company['jobs_list']
+            tag = company["jobs_list"]
             jobs = scrape_url(url)
-            jobs = jobs.find_all(tag['tag'], {'class': tag['class'], 'id': tag['id']})
+            jobs = jobs.find_all(tag["tag"], {"class": tag["class"], "id": tag["id"]})
             logger.info("Opening Jobs from {}".format(name))
             for job in jobs:
                 content = scrape_content(str(job))
@@ -23,30 +23,16 @@ def scrap_companies():
                 link = get_valid_link(path)
                 if not link:
                     link = url + path
-                item = content.get_text().strip().split('\n')
-                item = [itm.strip() for itm in item]
-                print("Job Link: {}".format(link))
-                for itm in item:
-                    if itm.strip():
-                        print(itm.strip())
-                if not company['job_info']:
-                    continue
-                job_info = company['job_info']
-                job_details = scrape_url(link)
-                job_details = job_details.find_all(job_info['tag'], {'class': job_info['class'], 'id': job_info['id']})
-                for info in job_details:
-                    details = info.get_text().strip().split('\n')
-                    for line in details:
-                        if line.strip():
-                            print(line.strip())
                 item = content.get_text().strip().split("\n")
+                item = [itm.strip() for itm in item if len(itm) > 0]
                 logger.info("Job info: {} \n Job link: \t {}".format(item, link))
         except:
             pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import time
-    curr_time = time.time()
+
+    start_time = time.time()
     scrap_companies()
-    print("Time spent: {} seconds".format(time.time()-curr_time))
+    logger.info("Time spent: {} seconds".format(time.time() - start_time))
